a029baeb8d0104976390d75e16414706649d9be5
cosmos1030 <78246188+cosmos1030@users.noreply.github.com>
2022-05-20 18:41:34 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var canvas = document.querySelector("canvas");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+c.fillStyle= '#D9D9D9';
+c.fillRect(118, 661, 82, 80);
+c.fillRect(200, 661, 82, 80);
+c.fillRect(282, 661, 82, 80);
+c.fillRect(364, 661, 82, 80);
+c.fillRect(528, 661, 82, 80);
+c.fillRect(610, 661, 82, 80);
+c.fillRect(692, 661, 82, 80);
+c.fillRect(774, 661, 82, 80);
+c.fillRect(938, 661, 82, 80);
+c.fillRect(1020, 661, 82, 80);
+c.fillRect(1184, 661, 82, 80);
+c.fillRect(1266, 661, 82, 80);
