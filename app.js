b6e98a0d7381f9b0a44017ee0bba2a18e962c0da
cosmos1030 <78246188+cosmos1030@users.noreply.github.com>
2022-05-21 18:18:32 +0900

    animation

--- app.js
+++ app.js
@@ -1,20 +1,72 @@
-var canvas = document.querySelector("canvas");
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var c = canvas.getContext('2d');
+const mouse = {
+    x: undefined,
+    y: undefined
+};
 
-c.fillStyle= '#D9D9D9';
-c.fillRect(118, 661, 82, 80);
-c.fillRect(200, 661, 82, 80);
-c.fillRect(282, 661, 82, 80);
-c.fillRect(364, 661, 82, 80);
-c.fillRect(528, 661, 82, 80);
-c.fillRect(610, 661, 82, 80);
-c.fillRect(692, 661, 82, 80);
-c.fillRect(774, 661, 82, 80);
-c.fillRect(938, 661, 82, 80);
-c.fillRect(1020, 661, 82, 80);
-c.fillRect(1184, 661, 82, 80);
-c.fillRect(1266, 661, 82, 80);
+addEventListener('mousemove', event=>{
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
+});
+
+addEventListener('resize', () => {
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+
+    init();
+});
+
+//objects
+function Circle(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = {
+        x: Math.random() - 0.5,
+        y: Math.random() - 0.5
+    };
+}
+
+Circle.prototype.draw = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    c.closePath();
+}
+
+Object.prototype.update = function() {
+    this.draw()
+
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+}
+
+let circles = [];
+function init() {
+    for(let i =0; i < 800; i++){
+        const x = Math.random() * canvas.width;
+        const y = Math.random() * canvas.height;
+        const radius = Math.random() * 5;
+        const color = 'blue';
+        circles.push(new Circle(x, y, radius, color))
+    }
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0,0, canvas.width, canvas.height);
+
+    circles.forEach(circle => {
+        circle.update();
+    });
+}
+
+init();
+animate();
